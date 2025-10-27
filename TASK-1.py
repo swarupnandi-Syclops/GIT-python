4c39f26c7babf6ec398cde276531eca4cfb3c98c
swarupnandi-Syclops <swarup.nandi@aot.edu.in>
2025-10-27 22:14:39 +0530

    Problem statement and code is added
    please review.

---
+++ TASK-1.py
@@ -0,0 +1,37 @@
+"""Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division"""
+
+#lets assume the iput numbers are 'a' and 'b' and the result is 's'
+
+#for addition
+
+a=float(input('enter the 1st value:'))
+b=float(input('enter the 2nd value:'))
+s=a+b
+print('the result is:',s)
+
+#for d+Substraction
+a=float(input('enter the 1st value:'))
+b=float(input('enter the 2nd value:'))
+s=a-b
+print('the result is:',s)
+
+#for multiplication
+a=float(input('enter the 1st value:'))
+b=float(input('enter the 2nd value:'))
+s=a*b
+print('the result is:',s)
+
+#for deletion
+a=float(input('enter the 1st value:'))
+b=float(input('enter the 2nd value:'))
+s=a/b
+print('the result is:',s)
+
+
